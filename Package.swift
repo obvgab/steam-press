baa2fa1886665650334d1eb85429aca4e3ab5443
Gab <gabriel@binarysky.ai>
2023-08-13 10:01:07 -0700

    first commit

---
+++ Package.swift
@@ -0,0 +1,34 @@
+// swift-tools-version:5.8
+import PackageDescription
+
+let package = Package(
+    name: "steam-engine",
+    platforms: [
+       .macOS(.v13)
+    ],
+    dependencies: [
+        // 💧 A server-side Swift web framework.
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.77.1"),
+        // 🗄 An ORM for SQL and NoSQL databases.
+        .package(url: "https://github.com/vapor/fluent.git", from: "4.8.0"),
+        // ἳ1 Fluent driver for Mongo.
+        .package(url: "https://github.com/vapor/fluent-mongo-driver.git", from: "1.0.0"),
+        // 🍃 An expressive, performant, and extensible templating language built for Swift.
+        .package(url: "https://github.com/vapor/leaf.git", from: "4.2.4"),
+    ],
+    targets: [
+        .executableTarget(
+            name: "App",
+            dependencies: [
+                .product(name: "Fluent", package: "fluent"),
+                .product(name: "FluentMongoDriver", package: "fluent-mongo-driver"),
+                .product(name: "Leaf", package: "leaf"),
+                .product(name: "Vapor", package: "vapor")
+            ]
+        ),
+        .testTarget(name: "AppTests", dependencies: [
+            .target(name: "App"),
+            .product(name: "XCTVapor", package: "vapor"),
+        ])
+    ]
+)
